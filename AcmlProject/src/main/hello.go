b3bdfd21ab64385fea26930d38f317f4725fe81e
naderwahba <nader.wahba96@gmail.com>
2018-12-01 18:04:02 +0200

    First Commit

---
+++ AcmlProject/src/main/hello.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+var tpl *template.Template
+
+func init (){
+	tpl = 	template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func main() {
+	http.HandleFunc("/",index)
+	http.HandleFunc("/process",processor)
+	http.ListenAndServe(":3000",nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request){
+	tpl.ExecuteTemplate(w,"index.html",nil)
+}
+
+func processor(w http.ResponseWriter , r *http.Request){
+	if r.Method != "POST"{
+		http.Redirect(w,r,"/",http.StatusSeeOther)
+		return
+	}
+	fname := r.FormValue("firster")
+	lname := r.FormValue("laster")
+
+	d := struct {
+		First string 
+		Last string 
+	}{
+		First: fname,
+		Last:lname,
+	}
+	tpl.ExecuteTemplate(w,"processor.html",d)
+}
