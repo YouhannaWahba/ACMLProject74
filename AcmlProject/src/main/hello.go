774d6fc0692351594739873bcfc306874764f0a4
Bishoy Raafat <bishoraafat10@gmail.com>
2018-12-03 14:15:25 +0200

    Added Calculate Mt GPA

--- AcmlProject/src/main/hello.go
+++ AcmlProject/src/main/hello.go
@@ -1,45 +1,50 @@
 package main
 
 import (
-	"net/http"
 	"html/template"
+	"net/http"
 )
 
 var tpl *template.Template
 
-func init (){
-	tpl = 	template.Must(template.ParseGlob("templates/*.html"))
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
 func main() {
-	http.HandleFunc("/",index)
-	http.HandleFunc("/maps",maps)
-	http.HandleFunc("/process",processor)
-	http.ListenAndServe(":3000",nil)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/maps", maps)
+	http.HandleFunc("/process", processor)
+	http.HandleFunc("/calculateGPA", calculatGPA)
+	http.ListenAndServe(":3000", nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request){
-	tpl.ExecuteTemplate(w,"index.html",nil)
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.html", nil)
 }
 
-func maps(w http.ResponseWriter, r *http.Request){
-	tpl.ExecuteTemplate(w,"maps.html",nil)
+func maps(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "maps.html", nil)
 }
 
-func processor(w http.ResponseWriter , r *http.Request){
-	if r.Method != "POST"{
-		http.Redirect(w,r,"/",http.StatusSeeOther)
+func calculatGPA(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "calculateGPA.html", nil)
+}
+
+func processor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 	fname := r.FormValue("firster")
 	lname := r.FormValue("laster")
 
 	d := struct {
-		First string 
-		Last string 
+		First string
+		Last  string
 	}{
 		First: fname,
-		Last:lname,
+		Last:  lname,
 	}
-	tpl.ExecuteTemplate(w,"processor.html",d)
+	tpl.ExecuteTemplate(w, "processor.html", d)
 }
